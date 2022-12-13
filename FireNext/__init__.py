6f366e64ca6c1c5ca9d9235cd6cfb8eb7ff53101
Mahfuj Salehin Moaz <almuaz1998@gmail.com>
2022-12-13 21:31:40 +0600

    main

--- FireNext/__init__.py
+++ FireNext/__init__.py
@@ -1,47 +1,23 @@
-"""
-MIT License
+from FireNext.Doc.__self__add__ import __self__add__
+from FireNext.Doc.__self__read__ import __self__read__
+from FireNext.Doc.__self__child__ import __self__child__
+from FireNext.Doc.__self__delete__ import __self__delete__
+from FireNext.Doc.__self__query__ import __self__query__
 
-Copyright (c) 2022 Mahfuz Salehin Moaz
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
-from FireNext.developer.Doc.__self__add__ import __self__add__
-from FireNext.developer.Doc.__self__read__ import __self__read__
-from FireNext.developer.Doc.__self__child__ import __self__child__
-from FireNext.developer.Doc.__self__delete__ import __self__delete__
-from FireNext.developer.Doc.__self__query__ import __self__query__
-
-from FireNext.developer.Table.__delete__table__ import __delete__table__
-from FireNext.developer.Table.__create__table__ import __create__table__
-from FireNext.developer.Table.__read__table__ import __read__table__
-from FireNext.developer.Table.__convert__table__doc__ import __convert__table__doc__
-from FireNext.developer.Table.__read__col__name__ import __read__col__name__
-from FireNext.developer.Table.__search__data__ import __search__data__
-from FireNext.developer.Table.__add__col__name__ import __add__col__name__
-from FireNext.developer.Table.__update__col__data__ import __update__col__data__
-from FireNext.developer.Table.__delete__col__name__ import __delete__col__name__
-from FireNext.developer.Table.__search__row__data__ import __search__row__data__
-from FireNext.developer.Table.__delete__row__ import __delete__row__
-from FireNext.developer.Table.__search__col__data__ import __search__col__data__
-from FireNext.developer.Table.__delete__col__ import __delete__col__
-from FireNext.developer.Table.__update__row__ import __update__row__
-from FireNext.developer.Table.__delete__data__ import __delete__data__
-from FireNext.developer.Table.__update__data__ import __update__data__
-from FireNext.developer.Table.__add__data__ import __add__data__
+from FireNext.Table.__delete__table__ import __delete__table__
+from FireNext.Table.__create__table__ import __create__table__
+from FireNext.Table.__read__table__ import __read__table__
+from FireNext.Table.__convert__table__doc__ import __convert__table__doc__
+from FireNext.Table.__read__col__name__ import __read__col__name__
+from FireNext.Table.__search__data__ import __search__data__
+from FireNext.Table.__add__col__name__ import __add__col__name__
+from FireNext.Table.__update__col__data__ import __update__col__data__
+from FireNext.Table.__delete__col__name__ import __delete__col__name__
+from FireNext.Table.__search__row__data__ import __search__row__data__
+from FireNext.Table.__delete__row__ import __delete__row__
+from FireNext.Table.__search__col__data__ import __search__col__data__
+from FireNext.Table.__delete__col__ import __delete__col__
+from FireNext.Table.__update__row__ import __update__row__
+from FireNext.Table.__delete__data__ import __delete__data__
+from FireNext.Table.__update__data__ import __update__data__
+from FireNext.Table.__add__data__ import __add__data__
